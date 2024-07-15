6bdde3f99de27eb9c98bca4bad080bca915c2805
Koketso Mawasha <65510296+KoketsoMawasha@users.noreply.github.com>
2024-07-15 17:01:07 +0200

    review changes

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
           <br /> <br />
           Step into K Graphics and discover how we can help you stand out on the road. Your journey to unique and impactful vehicle branding starts here.
           </p> 
-          <p className="text-md border border-green-600 w-fit py-2 px-4 bg-green-500 text-gray-800">Enquire on WhatsApp</p>
+          <a href='https://wa.me/+27670957766' className="text-md border border-green-600 w-fit py-2 px-4 bg-green-500 text-gray-800">Enquire on WhatsApp</a>
         </div>
 
         {/* Portfolio Section  */}
@@ -61,7 +61,7 @@ function App() {
             <div>
               <p className="text-xl font-semibold py-2">Contact</p>
               <p className="text-lg">067 095 7766</p>
-              <p className="text-lg">067 095 7766</p>
+              <p className="text-lg">078 506 9330</p>
             </div>
             <div className='text-left'>
               <p className="text-xl font-semibold py-2">Email</p>
@@ -77,16 +77,14 @@ function App() {
 
         <div className='grid grid-cols-1 md:grid-cols-3 px-8 py-20 text-left text-black '>
           <div className='place-content-center'><p className="text-lg font-bold ">Social Media Links</p></div>
-          <div className='place-self-start'>
+          <a href='https://www.instagram.com/mr_decals_za/' className='place-self-start'>
           <svg className='w-16' width="58" height="58" viewBox="0 0 198 199" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M197.97 99.5763C197.97 149.468 160.997 190.715 112.968 197.408C108.477 198.031 103.882 198.357 99.2181 198.357C93.8346 198.357 88.5479 197.928 83.3997 197.097C36.3732 189.525 0.459839 148.742 0.459839 99.5763C0.459839 45.019 44.6769 0.78894 99.2112 0.78894C153.746 0.78894 197.97 45.019 197.97 99.5763Z" fill="#1877F2"/>
           <path d="M112.968 80.1055V101.625H139.581L135.367 130.614H112.968V197.402C108.477 198.025 103.882 198.35 99.2181 198.35C93.8346 198.35 88.5479 197.921 83.3996 197.09V130.614H58.8553V101.625H83.3996V75.2949C83.3996 58.9595 96.637 45.7112 112.975 45.7112V45.7251C113.023 45.7251 113.064 45.7112 113.113 45.7112H139.588V70.7819H122.288C117.147 70.7819 112.975 74.9557 112.975 80.0986L112.968 80.1055Z" fill="white"/>
           </svg>
           <p className="text-sm font-bold py-2">Instagram</p>
-          
-
-          </div>
-          <div className='place-self-start'>
+          </a>
+          <a href='https://www.instagram.com/mr_decals_za/' className='place-self-start'>
           <svg width="auto" height="58" viewBox="0 0 198 199" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M99.2815 0.78894H98.8317C44.4151 0.78894 0.301758 44.9153 0.301758 99.3479V99.7978C0.301758 154.23 44.4151 198.357 98.8317 198.357H99.2815C153.698 198.357 197.811 154.23 197.811 99.7978V99.3479C197.811 44.9153 153.698 0.78894 99.2815 0.78894Z" fill="url(#paint0_linear_1_360)"/>
           <path d="M129.618 40.9837H68.5025C51.6184 40.9837 37.8828 54.7234 37.8828 71.6125V127.54C37.8828 144.429 51.6184 158.169 68.5025 158.169H129.618C146.502 158.169 160.237 144.429 160.237 127.54V71.6125C160.237 54.7234 146.502 40.9837 129.618 40.9837ZM48.6845 71.6125C48.6845 60.683 57.5763 51.7886 68.5025 51.7886H129.618C140.544 51.7886 149.436 60.683 149.436 71.6125V127.54C149.436 138.47 140.544 147.364 129.618 147.364H68.5025C57.5763 147.364 48.6845 138.47 48.6845 127.54V71.6125Z" fill="white"/>
@@ -102,7 +100,7 @@ function App() {
           </defs>
           </svg>
           <p className="text-sm font-bold py-2">Instagram</p>
-          </div>
+          </a>
         </div>
 
     </div>
